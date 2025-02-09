74468d57b064ebc842ec1b91e1269072dbbeaf40
japhet john <japhetjohnk@gmail.com>
2025-02-09 12:49:35 +0100

    done

--- front_end/script.js
+++ front_end/script.js
@@ -1,16 +1,4 @@
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-
-darkModeToggle.addEventListener('click', () => {
-document.body.classList.toggle('dark-mode');
-});
-
-document.querySelectorAll('nav a').forEach(link => {
-link.addEventListener('click', event => {
-event.preventDefault();
-const targetId = link.getAttribute('href');
-const targetSection = document.querySelector(targetId);
-const sections = document.querySelectorAll('main section');
-sections.forEach(section => section.style.display = 'none');
-targetSection.style.display = 'block';
-});
-});
+function toggleMenu() {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("active");
+}
